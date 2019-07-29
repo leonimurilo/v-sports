7728fe8406c9600e882a002d6fa7e5bcf24d2ea3
leoni <leonimega@hotmail.com>
2019-07-28 22:11:02 -0300

    call user preferences API

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -6,10 +6,19 @@ import { getUsers } from 'actions/users';
 // basically, intercepts every request action to perform a real api request
 export function* getUsersAsync() {
   try {
-    const { data } = yield call(api.getUsers);
-    yield put(getUsers.success(data));
+    const userResponse = yield call(api.getUsers);
+
+    // preferences data api should always return an array with the same length as the one that was sent
+    // and also return the data preserving the order of users
+    const prefResponse = yield call(api.getUsersPreferences, userResponse.data.map(u => u.email));
+
+    // merge data
+    const finalData = userResponse.data.map((user, index) => ({...user, ...prefResponse.data[index]}))
+
+    yield put(getUsers.success(finalData));
   } catch (error) {
     yield put(getUsers.failure(error));
+    console.error({error});
   }
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,8 @@
 import axios from 'axios';
 
 export const getUsers = () => axios.get('https://jsonplaceholder.typicode.com/users');
+
+// using post to avoid query parameter length restrictions in case of requesting preferences for many users
+export const getUsersPreferences = userList => {
+  return axios.post('http://localhost:9999/users/preferences', { users: userList });
+};
